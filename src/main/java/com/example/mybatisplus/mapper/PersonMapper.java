56fd98a26e6500e456b2f1cc2c1a1e0a97e880e0
zhuyanwu <ywzhup@isoftstone.com>
2021-01-29 23:30:57 +0800

    batch

--- src/main/java/com/example/mybatisplus/mapper/PersonMapper.java
+++ src/main/java/com/example/mybatisplus/mapper/PersonMapper.java
@@ -2,8 +2,16 @@ package com.example.mybatisplus.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.mybatisplus.vo.Person;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PersonMapper extends BaseMapper<Person> {
+
+     Integer insertBatch( @Param("list") List<Person> list);
+
+     Integer updateBatch (@Param("list") List<Person> list);
 }
